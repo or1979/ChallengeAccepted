2af6e3675dc6b72d349db08cec6a5c801dea9ee5
SydPRetzel <myskandev@gmail.com>
2019-04-25 19:19:39 -0400

    Structure, seeds and model for inspirations.

---
+++ models/seeds.sql
@@ -0,0 +1,36 @@
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (1,'\“Strength does not come from the physical capacity. It comes from an indomitable will.\”','Ghandi',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (2,'\“Training gives us an outlet for suppressed energies created by stress and thus tones the spirit just as exercise conditions the body.\”','Arnold Schwarzenegger',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (3,'\“Your body can stand almost anything. It\'s your mind that you have to convince.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (4,'\“Fitness is like a relationship. You can\'t cheat and expect it to work.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (5,'\“My protein shake brings the boys to the yard. And they\'re like: your muscles are hard. Damn right, my muscles are hard. I could train ya, but I\'d have to charge.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (6,'\“Respect your body. It\'s the only one you get.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (7,'\“Dead last finish is greater than did not finish, which trumps did not start.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (8,'\“The best way to predict the future is to create it.\”','Abraham Lincoln',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (9,'\“Work hard in silence. Let success be your noise.\”','Frank Ocean',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (10,'\“Rome wasn\'t built in a day, but they worked on it every single day.\”','Unknown',"http://",NOW(),NOW());
+
+
+
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (11,'\“Use the 21/90 rule: It takes 21 days to create a habit. It takes 90 days to create a lifestyle.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (12,'\“Strive for progress, not perfection.\”','Unknown',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (13,'\“The difference between try and triumph is a little umph.\”','Marvin Phillips',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (14,'\“It\'s funny how humans can wrap their mind around things and fit them into their version of reality.\”','Rick Riordan, The Lightning Thief',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (15,'\“The beginning of love is the will to let those we love be perfectly themselves, the resolution not to twist them to fit our own image.\”','Thomas Merton, The Way of Chuang Tzu',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (16,'\“Religion is like a pair of shoes.....Find one that fits for you, but don\’t make me wear your shoes.\”','George Carlin',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (17,'\“A fit, healthy body—that is the best fashion statement\”','Jess C Scott',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (18,'\“I must not fear. Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me. And when it has gone past I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.\”','Frank Herbert, Dune',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (19,'\“The truth is, unless you let go, unless you forgive yourself, unless you forgive the situation, unless you realize that the situation is over, you cannot move forward.\”','Steve Maraboli, Unapologetically You: Reflections on Life and the Human Experience',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (20,'\“Attitude is a choice. Happiness is a choice. Optimism is a choice. Kindness is a choice. Giving is a choice. Respect is a choice. Whatever choice you make makes you. Choose wisely.\”','Roy T. Bennett, The Light in the Heart',"http://",NOW(),NOW());
+
+
+
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (21,'\“You never change your life until you step out of your comfort zone; change begins at the end of your comfort zone.\”','Roy T. Bennett',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (22,'\“The man who moves a mountain begins by carrying away small stones.\”','Confucius, Confucius: The Analects',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (23,'\“Without ambition one starts nothing. Without work one finishes nothing. The prize will not be sent to you. You have to win it.\"','Ralph Waldo Emerson',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (24,'\“Do not stop thinking of life as an adventure. You have no security unless you can live bravely, excitingly, imaginatively; unless you can choose a challenge instead of competence.\”','Eleanor Roosevelt, The Autobiography of Eleanor Roosevelt',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (25,'\"I don\'t have weekdays on my calendar, only strong days,\"','Dom Mezzetti',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (26,'\"You have no responsibility to live up to what other people think you ought to accomplish.  I have no responsibility to be like they expect me to be.  It\'s their mistake, not my failing.\"','Richard P. Feynman',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (27,'\"All the time you\'re saying to yourself, \"I could do that, but I won\'t,\" which is just another way of saying that you can\'t.\"','Richard P. Feynman',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (28,'\"Be yourself; everyone else is already taken.\"','Oscar Wilde',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (29,'\"Whether you think you can, or you think you can\'t--you\'re right\"','Henry Ford',"http://",NOW(),NOW());
+INSERT INTO inspirations (day_id,quote,author,image_url,createdAt,updatedAt) VALUES (30,'\"Do or do not.  There is no try\"','Jedi Grand Master Yoda',"http://",NOW(),NOW());
