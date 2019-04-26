fbb74cf71152592a74cd8b2d85ad8aa4ba06eb6e
herbiemolina <herbiemolina@gmail.com>
2019-04-26 11:30:56 -0400

    self report form

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -13,10 +13,41 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
+    });
+  });
+
+  // Load index page
+  app.get("/report", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("report", {
+        msg: "Welcome to the Survey page! How did you do today?",
+        bmi:  <!-- rbpm -->
+        <div class="modal fade" id="rbpm" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalScrollableTitle">What's Your resting beats per minute?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                ...things and stufffsss
+                <input type="text" class="form-control" id="rbpm" placeholder="What's your resting heart rate?">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+            </div>
+        </div>
+        </div>      });
     });
   });
 
