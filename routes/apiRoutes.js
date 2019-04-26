a2c6ddafb6752ce1e06ec727ffd742ca9a4b72ee
SydPRetzel <myskandev@gmail.com>
2019-04-26 00:57:03 -0400

    RBPM chart initial pass.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,49 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
+
+  // Send the metrics to the completed page.
+  app.get("/api/metrics", function (req, res) {
+    console.log("/api/metrics");
+    db.Metric.findAll({}).then(function (results) {
+      //console.log(results);
+      let retObj = {
+        day_id: [],
+        rbpm: [],
+        bmi: [],
+        blog: []
+      }
+      for (let i = 0; i < results.length; i++) {
+        //console.log("id: " + i + " " + results[i].day_id);
+        retObj.day_id.push(results[i].day_id);
+        retObj.rbpm.push(results[i].rbpm);
+        retObj.bmi.push(results[i].bmi);
+        retObj.blog.push(results[i].blog);
+      }
+      //console.log(JSON.stringify(retObj));
+
+      res.json(retObj);
+    });
+  });
+
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,9 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -11,9 +11,15 @@ module.exports = function(app) {
     });
   });
 
+  // Load the challenge completed page.
+  app.get("/completed", function (req, res) {
+    console.log("/completed");
+    res.render("completed", {});
+  });
+
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
@@ -21,7 +27,7 @@ module.exports = function(app) {
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
